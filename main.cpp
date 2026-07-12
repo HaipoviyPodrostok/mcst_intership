55f9d827591bf2580ea94ee19eb9dbf5428c5810
HaipoviyPodrostok <alexkurteev228@mail.ru>
2026-07-12 10:36:56 +0300

    beta

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <cstdint>
 #include <iostream>
-#include <stdexcept>
 #include <string>
 
 #include "graph.hpp"
@@ -9,6 +8,7 @@
 #ifdef ENABLE_DUMP
   #include <filesystem>
   #include <fstream>
+
 inline void GraphDump(const Graph& graph) {
   static uint64_t   dump_counter = 1;
   const std::string dir_path     = "dumps/dots";
@@ -33,57 +33,58 @@ int main() {
   while (std::cin >> command) {
     try {
       if (command == "NODE") {
-      std::string id;
-      std::cin >> id;
-      graph.AddNode(id);
-    }
-    else if (command == "EDGE") {
-      std::string from, to;
-      uint64_t    weight;
-      std::cin >> from >> to >> weight;
-      graph.AddEdge(from, to, weight);
-    }
-    else if (command == "REMOVE") {
-      std::string sub_command;
-      std::cin >> sub_command;
-
-      if (sub_command == "NODE") {
         std::string id;
         std::cin >> id;
-        graph.RemoveNode(id);
+        graph.AddNode(id);
       }
-      else if (sub_command == "EDGE") {
+      else if (command == "EDGE") {
         std::string from, to;
-        std::cin >> from >> to;
-        graph.RemoveEdge(from, to);
+        uint64_t    weight;
+        std::cin >> from >> to >> weight;
+        graph.AddEdge(from, to, weight);
       }
-    }
+      else if (command == "REMOVE") {
+        std::string sub_command;
+        std::cin >> sub_command;
+
+        if (sub_command == "NODE") {
+          std::string id;
+          std::cin >> id;
+          graph.RemoveNode(id);
+        }
+        else if (sub_command == "EDGE") {
+          std::string from, to;
+          std::cin >> from >> to;
+          graph.RemoveEdge(from, to);
+        }
+      }
 
 #ifdef ENABLE_DUMP
-    else if (command == "DUMP") { GraphDump(graph); }
+      else if (command == "DUMP") { GraphDump(graph); }
 #endif  // ENABLE_DUMP
 
-    else if (command == "RPO_NUMBERING") {
-      std::string id;
-      std::cin >> id;
-      RunRpoNumbering(graph, id);
-    }
-    else if (command == "DIJKSTRA") {
-      std::string id;
-      std::cin >> id;
-      RunDijkstra(graph, id);
-    }
-    else if (command == "MAXFLOW") {
-      std::string from, to;
-      std::cin >> from >> to;
-      RunMaxFlow(graph, from, to);
-    }
-    else if (command == "TARJAN") {
-      std::string id;
-      std::cin >> id;
+      else if (command == "RPO_NUMBERING") {
+        std::string id;
+        std::cin >> id;
+        RunRpoNumbering(graph, id);
+      }
+      else if (command == "DIJKSTRA") {
+        std::string id;
+        std::cin >> id;
+        RunDijkstra(graph, id);
+      }
+      else if (command == "MAXFLOW") {
+        std::string from, to;
+        std::cin >> from >> to;
+        RunMaxFlow(graph, from, to);
+      }
+      else if (command == "TARJAN") {
+        std::string id;
+        std::cin >> id;
         RunTarjan(graph, id);
       }
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception& e) {
       std::cout << e.what() << "\n";
     }
   }
